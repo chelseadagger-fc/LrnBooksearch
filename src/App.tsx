9a2fe61aac3b543f22081027f0703256a1819d33
Daniel <daniel.aldridge@alumni.unc.edu>
2023-06-18 17:41:53 +0300

    created expandable text component

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,18 @@
 
-import Like from "./components/Like";
+import ExpandableText from "./components/ExpandableText"
 
 function App() {
+  
+
 
   return (
     <div>
-      <Like onClick={() => console.log("clicked")}/>
+      <ExpandableText maxChars={100}>
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore 
+      magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo 
+      consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. 
+      Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+      </ExpandableText>
     </div>
   )
 }
