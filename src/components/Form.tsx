b7a2348781cac16a4cc75dc526be814e504d935e
Daniel <daniel.aldridge@alumni.unc.edu>
2023-06-21 16:09:14 +0300

    rewrote form component using react-hook-form

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -1,26 +1,20 @@
-import { FormEvent, useState } from 'react'
+import { FieldValues, useForm } from 'react-hook-form'
 
 const Form = () => {
 
-  const [person, setPerson] = useState({
-    name: '',
-    age: 0
-  })
+  const { register, handleSubmit } = useForm();
 
-  const handleSubmit = (event: FormEvent) => {
-    event.preventDefault();
-    console.log(person);
-  };
+  const onSubmit = (data: FieldValues) => console.log(data);
   
   return (
-    <form onSubmit={handleSubmit}>
+    <form onSubmit={handleSubmit(onSubmit)}>
         <div className="mb-3">
             <label htmlFor="name" className="form-label">Name</label>
-            <input onChange={(event) => setPerson({...person, name: event.target.value})} value={person.name} id="name" type="text" className="form-control" />
+            <input {...register('name')} id="name" type="text" className="form-control" />
         </div>
         <div className="mb-3">
             <label htmlFor="age" className="form-label">Age</label>
-            <input onChange={(event) => setPerson({...person, age: parseInt(event.target.value)})} value={person.age} id="age" type="number" className="form-control" />
+            <input {...register('age')} id="age" type="number" className="form-control" />
         </div>
         <button className="btn btn-primary" type="submit">Submit</button>
     </form>
